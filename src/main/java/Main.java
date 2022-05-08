48460143b1436199ff9ee36b11a76eff96a0367d
redsun9 <mokeev.vladimir@gmail.com>
2022-05-09 01:53:13 +0300

    add handling of concurrent clients for pings

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,37 +11,63 @@ public class Main {
 
         //  Uncomment this block to pass the first stage
         ServerSocket serverSocket = null;
-        Socket clientSocket = null;
-        InputStream inputStream = null;
-        OutputStream outputStream = null;
         int port = 6379;
         try {
             serverSocket = new ServerSocket(port);
             serverSocket.setReuseAddress(true);
             // Wait for connection from client.
-            clientSocket = serverSocket.accept();
-            inputStream = clientSocket.getInputStream();
-            outputStream = clientSocket.getOutputStream();
-            int available;
-            while (true) {
-                available = inputStream.available();
-                if (available > 0) {
-                    byte[] inputBytes = new byte[available];
-                    int read = 0;
-                    while (read < available) read += inputStream.read(inputBytes, read, available - read);
-                    byte[] outputBytes = "+PONG\r\n".getBytes();
-                    outputStream.write(outputBytes);
-                }
+            while (!serverSocket.isClosed() && !Thread.currentThread().isInterrupted()) {
+                Socket clientSocket = serverSocket.accept();
+                // Create a new thread for the client.
+                new Thread(new ClientHandler(clientSocket)).start();
             }
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
         } finally {
             try {
-                if (clientSocket != null) {
-                    clientSocket.close();
+                if (serverSocket != null) serverSocket.close();
+            } catch (IOException e) {
+                System.out.println("IOException: " + e.getMessage());
+            }
+        }
+    }
+
+    private static class ClientHandler implements Runnable {
+        private Socket clientSocket;
+        private InputStream inputStream;
+        private OutputStream outputStream;
+        private int available;
+        private byte[] inputBytes;
+        private byte[] outputBytes;
+        private int read;
+
+        public ClientHandler(Socket clientSocket) {
+            this.clientSocket = clientSocket;
+        }
+
+        @Override
+        public void run() {
+            try {
+                inputStream = clientSocket.getInputStream();
+                outputStream = clientSocket.getOutputStream();
+                while (!clientSocket.isClosed()) {
+                    available = inputStream.available();
+                    if (available > 0) {
+                        inputBytes = new byte[available];
+                        read = 0;
+                        while (read < available) read += inputStream.read(inputBytes, read, available - read);
+                        outputBytes = "+PONG\r\n".getBytes();
+                        outputStream.write(outputBytes);
+                    }
                 }
             } catch (IOException e) {
                 System.out.println("IOException: " + e.getMessage());
+            } finally {
+                try {
+                    if (clientSocket != null) clientSocket.close();
+                } catch (IOException e) {
+                    System.out.println("IOException: " + e.getMessage());
+                }
             }
         }
     }
